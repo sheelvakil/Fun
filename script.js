58add6ad50a9b76d2cad0fe757ea29ceb1c954ec
sheelvakil <65753216+sheelvakil@users.noreply.github.com>
2022-06-10 21:54:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function btntest_onclick(){
+  if (document.getElementById('input-code').value == '1234') {
+      window.location.href = "http://www.google.com";
+  } else {
+      window.location.href = "http://www.bing.com";
+  }
+}
